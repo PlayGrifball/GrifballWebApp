d6ac8ea8cc2ff3af544c767d9537e5dcf7775cbf
Noah Surprenant <noah.surprenant@gmail.com>
2024-03-15 23:18:39 -0400

    Create seeder project

---
+++ GrifballWebApp.Seeder/HostedService.cs
@@ -0,0 +1,31 @@
+﻿using GrifballWebApp.Database;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrifballWebApp.Seeder;
+internal class HostedService : BackgroundService
+{
+    private readonly IServiceProvider _serviceProvider;
+
+    public HostedService(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken ct)
+    {
+        using var scope = _serviceProvider.CreateScope();
+        using var context = scope.ServiceProvider.GetRequiredService<GrifballContext>();
+
+        await context.Database.EnsureDeletedAsync(ct);
+        await context.Database.MigrateAsync(ct);
+
+
+    }
+}
---
+++ GrifballWebApp.Seeder/Program.cs
@@ -0,0 +1,19 @@
+﻿using GrifballWebApp.Database;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace GrifballWebApp.Seeder;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Host.CreateDefaultBuilder(args).ConfigureServices((hostContext, services) =>
+        {
+            services.AddDbContext<GrifballContext>((services, options) => options.UseSqlServer(services.GetRequiredService<IConfiguration>().GetConnectionString("GrifballWebApp") 
+                ?? throw new Exception("GrifballWebApp connection string missing")));
+        }).Build().Run();
+    }
+}
