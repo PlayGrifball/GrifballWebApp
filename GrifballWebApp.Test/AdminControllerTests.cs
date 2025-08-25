593bf6006d9cc76a4f2db43365c72a7a2c22ee1e
Noah Surprenant <noah.surprenant@gmail.com>
2025-08-24 23:00:38 -0400

    Add tests

---
+++ GrifballWebApp.Test/AdminControllerTests.cs
@@ -0,0 +1,94 @@
+using GrifballWebApp.Server.Controllers;
+using GrifballWebApp.Server.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using NSubstitute;
+using NSubstitute.ExceptionExtensions;
+using Surprenant.Grunt.Core;
+using Surprenant.Grunt.Core.Storage;
+using Surprenant.Grunt.Models;
+using Surprenant.Grunt.Models.HaloInfinite;
+
+namespace GrifballWebApp.Test;
+
+[TestFixture]
+[FixtureLifeCycle(LifeCycle.InstancePerTestCase)]
+public class AdminControllerTests
+{
+    private ILogger<AdminController> _logger;
+    private IHaloInfiniteClientFactory _haloInfiniteClientFactory;
+    private IAccountAuthorization _accountAuthorization;
+    private IDataPullService _dataPullService;
+    private IOptionsMonitor<ClientConfiguration> _options;
+    private AdminController _controller;
+
+    [SetUp]
+    public void Setup()
+    {
+        _logger = Substitute.For<ILogger<AdminController>>();
+        _haloInfiniteClientFactory = Substitute.For<IHaloInfiniteClientFactory>();
+        _accountAuthorization = Substitute.For<IAccountAuthorization>();
+        _dataPullService = Substitute.For<IDataPullService>();
+        _options = Substitute.For<IOptionsMonitor<ClientConfiguration>>();
+        _controller = new AdminController(_logger, _haloInfiniteClientFactory, _accountAuthorization, _dataPullService, _options);
+    }
+
+    [Test]
+    public void Get_ShouldThrowException_WhenGameIsNotGuid()
+    {
+        var ex = Assert.ThrowsAsync<Exception>(async () => await _controller.Get("not-a-guid"));
+        Assert.That(ex.Message, Is.EqualTo("Not valid guid"));
+    }
+
+    [Test]
+    public async Task Get_ShouldCallDataPullService_WhenGuidIsValid()
+    {
+        var guid = Guid.NewGuid();
+        await _controller.Get(guid.ToString());
+        await _dataPullService.Received(1).GetAndSaveMatch(guid);
+    }
+
+    [Test]
+    public async Task SetCode_ShouldReturnBadRequest_WhenCodeIsMissing()
+    {
+        var result = await _controller.SetCode("");
+        Assert.That(result, Is.TypeOf<BadRequestObjectResult>());
+    }
+
+    [Test]
+    public async Task SetCode_ShouldCallSetCodeAsync_AndReturnOk()
+    {
+        var code = "testcode";
+        var result = await _controller.SetCode(code);
+        await _accountAuthorization.Received(1).SetCodeAsync(code);
+        Assert.That(result, Is.TypeOf<OkResult>());
+    }
+
+    [Test]
+    public void DeleteTokens_ShouldReturnOk()
+    {
+        var result = _controller.DeleteTokens();
+        Assert.That(result, Is.TypeOf<OkResult>());
+    }
+
+    [Test]
+    public async Task CheckStatus_ShouldReturnGood_WhenStatsGetMatchStatsSucceeds()
+    {
+        _haloInfiniteClientFactory.StatsGetMatchStats(Arg.Any<string>())
+            .Returns(new HaloApiResultContainer<MatchStats, HaloApiErrorContainer>(new MatchStats(), null));
+        var result = await _controller.CheckStatus();
+        Assert.That(result, Is.EqualTo("Good"));
+    }
+
+    [Test]
+    public async Task CheckStatus_ShouldReturnAuthUrl_WhenStatsGetMatchStatsThrows()
+    {
+        _haloInfiniteClientFactory.StatsGetMatchStats(Arg.Any<string>())
+            .ThrowsAsync(callInfo => throw new Exception("fail"));
+        var config = new ClientConfiguration { ClientId = "cid", RedirectUrl = "http://redir" };
+        _options.CurrentValue.Returns(config);
+        var result = await _controller.CheckStatus();
+        Assert.That(result, Is.EqualTo("https://login.live.com/oauth20_authorize.srf?client_id=cid&response_type=code&approval_prompt=auto&scope=Xboxlive.signin+Xboxlive.offline_access&redirect_uri=http%3a%2f%2fredir"));
+    }
+}
---
+++ GrifballWebApp.Test/UserManagementServiceTests.cs
@@ -0,0 +1,106 @@
+using GrifballWebApp.Database;
+using GrifballWebApp.Database.Models;
+using GrifballWebApp.Server.UserManagement;
+using GrifballWebApp.Server.Services;
+using GrifballWebApp.Server.Dtos;
+using Microsoft.AspNetCore.Identity;
+using NSubstitute;
+using Surprenant.Grunt.Core;
+
+namespace GrifballWebApp.Test;
+
+[TestFixture]
+[FixtureLifeCycle(LifeCycle.InstancePerTestCase)]
+public class UserManagementServiceTests
+{
+    private GrifballContext _context;
+    private UserManagementService _service;
+    private IHaloInfiniteClientFactory _haloInfiniteClientFactory;
+    private UserManager<User> _userManager;
+    private IGetsertXboxUserService _getsertXboxUserService;
+
+    [SetUp]
+    public async Task Setup()
+    {
+        _context = await SetUpFixture.NewGrifballContext();
+        _haloInfiniteClientFactory = Substitute.For<IHaloInfiniteClientFactory>();
+        _userManager = Substitute.For<UserManager<User>>(
+            Substitute.For<IUserStore<User>>(), null, null, null, null, null, null, null, null);
+        _getsertXboxUserService = Substitute.For<IGetsertXboxUserService>();
+        _service = new UserManagementService(_context, _haloInfiniteClientFactory, _userManager, _getsertXboxUserService);
+    }
+
+    [TearDown]
+    public void TearDown() => _context.Dispose();
+
+    [Test]
+    public async Task GetUsers_ShouldReturnPaginatedUsers()
+    {
+        _context.Users.Add(new User { UserName = "TestUser", DisplayName = "TestDisplay" });
+        await _context.SaveChangesAsync();
+        var filter = new PaginationFilter { PageNumber = 1, PageSize = 10 };
+        var result = await _service.GetUsers(filter, string.Empty, CancellationToken.None);
+        Assert.That(result.Results.Length, Is.GreaterThan(0));
+        Assert.That(result.Results[0].UserName, Is.EqualTo("TestUser"));
+    }
+
+    [Test]
+    public async Task GetUser_ShouldReturnUser_WhenExists()
+    {
+        var user = new User { UserName = "TestUser", DisplayName = "TestDisplay" };
+        _context.Users.Add(user);
+        await _context.SaveChangesAsync();
+        var result = await _service.GetUser(user.Id, CancellationToken.None);
+        Assert.That(result, Is.Not.Null);
+        Assert.That(result.UserName, Is.EqualTo("TestUser"));
+    }
+
+    [Test]
+    public async Task CreateUser_ShouldReturnError_WhenUserNameTaken()
+    {
+        _context.Users.Add(new User { UserName = "TestUser" });
+        await _context.SaveChangesAsync();
+        var dto = new CreateUserRequestDto { UserName = "TestUser", Gamertag = "GT", DisplayName = "Display" };
+        var result = await _service.CreateUser(dto, CancellationToken.None);
+        Assert.That(result, Is.EqualTo("User name is taken"));
+    }
+
+    [Test]
+    public async Task EditUser_ShouldUpdateUserProperties()
+    {
+        var user = new User { UserName = "TestUser", DisplayName = "OldDisplay" };
+        _context.Users.Add(user);
+        await _context.SaveChangesAsync();
+        _userManager.SetUserNameAsync(user, "NewUser").Returns(IdentityResult.Success);
+        _userManager.SetLockoutEndDateAsync(user, null).Returns(IdentityResult.Success);
+        _userManager.SetLockoutEnabledAsync(user, false).Returns(IdentityResult.Success);
+        _userManager.GetRolesAsync(user).Returns(new List<string>());
+        _userManager.AddToRolesAsync(user, Arg.Any<IEnumerable<string>>()).Returns(IdentityResult.Success);
+        _userManager.RemoveFromRolesAsync(user, Arg.Any<IEnumerable<string>>()).Returns(IdentityResult.Success);
+
+        var xboxUser = new XboxUser { Gamertag = "GT", XboxUserID = 1 };
+        await _context.XboxUsers.AddAsync(xboxUser);
+        await _context.SaveChangesAsync();
+        _getsertXboxUserService.GetsertXboxUserByGamertag(Arg.Is("GT"), Arg.Any<CancellationToken>()).Returns((xboxUser, null));
+        var editDto = new UserResponseDto
+        {
+            UserID = user.Id,
+            UserName = "NewUser",
+            DisplayName = "NewDisplay",
+            IsDummyUser = true,
+            LockoutEnd = null,
+            LockoutEnabled = false,
+            Gamertag = "GT",
+            Region = "NA",
+            Discord = "discord",
+            ExternalAuthCount = 0,
+            Roles = new List<RoleDto> { new RoleDto { RoleName = "Admin", HasRole = true } }
+        };
+        var result = await _service.EditUser(editDto, CancellationToken.None);
+        var updated = await _service.GetUser(user.Id, CancellationToken.None);
+        Assert.That(result, Is.Null);
+        Assert.That(updated, Is.Not.Null);
+        Assert.That(updated.DisplayName, Is.EqualTo("NewDisplay"));
+        //Assert.That(updated.UserName, Is.EqualTo("NewUser")); // User manager is mocked rn so the username is not updated...
+    }
+}
